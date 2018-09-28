0dde596fdb52d7e88379fc757c091cdfe200a8c8
FanSilver <pnmtrung106@gmail.com>
2018-09-28 17:06:44 +0700

    test

--- app/src/main/java/capstone1/dtu/f4/sosapplication/Login_Activity.java
+++ app/src/main/java/capstone1/dtu/f4/sosapplication/Login_Activity.java
@@ -3,12 +3,11 @@ package capstone1.dtu.f4.sosapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Login_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_login_);
     }
 }
